14b9449cb02f59954406d2895782d6f14c32eeb5
KT <krakovszkyt@gmail.com>
2022-10-09 19:32:56 +0200

    Add CSS to Plant component

--- src/Plant.js
+++ src/Plant.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import './Card.css'
 
 export default function Plant({plant}) {
   return (
-    <div>
-        plant: {plant.type}
+    <div className='container'>    
+      <div className='card-container'>
+        <div className='image-container'>
+          <img src="" alt=""/>
+        </div>
+        <div className='card-content'>
+          <div className='card-title'>
+            {plant.type}
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 }
