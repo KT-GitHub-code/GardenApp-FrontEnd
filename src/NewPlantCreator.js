3cdcb53e8028cd59f7fdc3dee8be46a8b47f7d61
KT <krakovszkyt@gmail.com>
2022-10-18 14:06:22 +0200

    Add form with select to NewPlantCreator

--- src/NewPlantCreator.js
+++ src/NewPlantCreator.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const NewPlantCreator = () => {
+
+  const [myPlant, setMyPlant] = useState("ALOE");
+
+  const handleChange = (event) => {
+    setMyPlant(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Your new ${myPlant} plant was sent to your Garden!`)
+  }
+
   return (
-    <div>NewPlantCreator</div>
+    <form onSubmit={handleSubmit}>
+      <label>Select plant type:
+        <select value={myPlant} onChange={handleChange}>
+          <option selected value="ALOE">Aloe</option>
+          <option value="SEDUM" selected>Sedum</option>
+        </select>
+      </label>
+      <button type='submit'>Add plant</button>
+    </form>
   )
 }
 
